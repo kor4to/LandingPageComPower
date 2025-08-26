4d64982dae6ad883d46c901c8b433a74073c9dba
Oren Arones Paz <oren.arones@outlook.com>
2025-08-26 17:56:17 -0500

    Servicios 26/08

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,13 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
+  server: {
+    port: 4321,
+    host: '0.0.0.0',
+  },
+
+
+
   vite: {
     plugins: [tailwindcss()],
   },
